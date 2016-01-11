634864d638af6c3dbc0f83f353e070db8e980dae
dhvogel <dhvogel2468@gmail.com>
2016-01-11 15:30:46 -0500

    TableView progress

--- WatervilleFood/MenuTableViewController.swift
+++ WatervilleFood/MenuTableViewController.swift
@@ -27,14 +27,14 @@ class MenuTableViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return (array?.count)!
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! CustomTableViewCell
         
-        cell.labUerName.text = "Name"
-        cell.labMessage.text = "Message \(indexPath.row)"
+        cell.labUerName.text = array![indexPath.row].valueForKey("Item") as! String!
+        cell.labMessage.text = "$\(String.localizedStringWithFormat("%.2f %@", array![indexPath.row].valueForKey("Price") as! Double!,""))"
         cell.labTime.text = NSDateFormatter.localizedStringFromDate(NSDate(), dateStyle: .ShortStyle, timeStyle: .ShortStyle)
         
         return cell
